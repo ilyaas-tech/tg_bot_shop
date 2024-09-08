594e5e54805f054d73b9e910416522c0a3965650
ilyaas-tech <aliev2010ilyas@gmail.com>
2024-09-08 12:01:20 +0300

    add buttons

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import asyncio
+import os
 from aiogram import Bot, Dispatcher
 
 from app.handlers import router
 
 from app.database.models import async_main
 
-
+from dotenv import load_dotenv
+load_dotenv()
 async def main():
     await async_main()
-    bot = Bot(token='7321221051:AAFtWM0S5DXLpojwDWFoTMj8PSJ-TVowfAs')
+    bot = Bot(token=os.getenv('BOT_TOKEN'))
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
